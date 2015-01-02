80779a12df1dc7c4c1477bf54e2f7a44e49f6ad3
Xvezda <xvezda@naver.com>
2015-01-02 22:33:35 +0900

    mistype fixed

--- tpl/js/sitemap.js
+++ tpl/js/sitemap.js
@@ -2,7 +2,7 @@ jQuery(function($) {
 	$('a[href="#ping"]').click(function() {
 		var response_tags = new Array('error','message','result');
 		var params = new Array();
-		exec_xml('cache', 'procSitemapAdminPingSitemap', params, function(ret_obj,response_tags){
+		exec_xml('sitemap', 'procSitemapAdminPingSitemap', params, function(ret_obj,response_tags){
 		var error = ret_obj['error'];
 		var message = ret_obj['message'];
 		alert(message);
